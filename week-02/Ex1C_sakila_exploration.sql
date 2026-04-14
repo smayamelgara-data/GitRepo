793877e4312247a5ea35fa535ca36e39e8689adb
smayamelgara-data <smayamelgara@gmail.com>
2026-04-14 18:37:59 -0400

    Exercise 1.C Complete

---
+++ week-02/Ex1C_sakila_exploration.sql
@@ -0,0 +1,36 @@
+/*
+a) In the schema panel, expand the table for actor and then expand Columns. What
+information is included in this table?
+	Actor ID, their first name, last name, and last update , so this is the actors 
+    personal information
+    
+b) Now expand the table for film and then expand Columns. What information is
+included in this table?
+	The films information, id, titile, description, year it was released, 
+    language and everything else.
+
+c) Which other table in sakila contains columns for both actor_id and film_id?
+	The film_actor table contains both. 
+
+d) Right-click on rental and choose the menu option for Select Rows. Notice that
+this opens a new query window. Look at the query results to determine what
+information you can find in the rental table. What does this table include? Is this
+information easy to read, or hard to read? Why?
+	Its rental information such as rental, inventory, customer, and staff id. As well
+    as inventory, customer and staff id. The problem with this is that its just a bunch of 
+    numbers which would become hard to read. Making sure to provide customer or rental name 
+    could possibly be easier to read for others. Which would also be easier to identify what 
+    the chart is telling me. 
+
+e) Perform the same steps for the inventory table. What does this table include?
+	The inventory table includes Inventory, film and store id as well as last update. This 
+    would help us identify the amount of merchandize we have and where. 
+
+f) Based on what you have learned so far, which tables do you need to use in order to
+understand the names of all films that were rented on a specific date? How are
+these tables related to each other? Answer with an explanation in full sentences
+	In order to understand the names of all films, I would use the film tab and columns chart. 
+    This table would give you when the film was released and their rental rate so you
+    can compare their relation by their description, rating or special features. 
+
+*/

4604e5a9857f536f7ace0fc73e84a35390a8fc84
smayamelgara-data <smayamelgara@gmail.com>
2026-04-14 10:05:10 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,10 @@
 /*
 Sakila is a database used to collect information about ____ and ____ for use by _____.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved [number] records
+
+SELECT * FROM film;
+
+SELECT * FROM film; -- Retrieved [number] records

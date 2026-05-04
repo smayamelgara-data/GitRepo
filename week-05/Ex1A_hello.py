e4bc6fa39bef21e8f243c59a595495a67ef7fd2e
smayamelgara-data <smayamelgara@gmail.com>
2026-05-04 14:53:57 -0400

    Adding Exercise1A Pt2 Python File.

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,24 @@
 print('Hello world!')
+
+
+message = 'Hello World!'
+print(message)
+
+#Hello world prints twice because both prints are being ran.
+#If you were to comment a print out then only one would print. 
+
+
+#Displaying dollars and cents 
+dollars = 3
+cents = .50
+print (dollars + cents)
+cents = cents +.25
+print (dollars + cents)
+#The result is only showing one decimal place. I did not set the variable to have two decimal places.
+
+#Once i added the .25, it seems like it will print the second decimal place 
+#if the number is not0.
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " "+ c_str)

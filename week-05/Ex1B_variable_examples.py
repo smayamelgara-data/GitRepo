194e31a97f172ac11deaf28090307934b1423255
smayamelgara-data <smayamelgara@gmail.com>
2026-05-05 09:51:48 -0400

    Exercise 1B Variable_Example.py File.

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,41 @@
+#Exercise 1B File 
+#Smaya Melgara 
+
+#Creating the variables 
+customer_id = int
+customer_name = str
+customer_gender = str
+customer_dob = str
+drivers_license_num = int
+auto_policy_num  = int  
+
+#Setting variables to a value 
+customer_id = 803
+ #I wrote the id as three digits but maybe it could be less or more 
+customer_name = 'Smaya M' 
+# I wrote my name and first initial but maybe it could be First and last name or just the first name 
+customer_gender = 'Female' 
+# I wrote out Female but maybe it could be just the first initial  
+customer_dob= 'June 23 2003'
+ #I wrote it as a string but it could also be an interger 
+drivers_license_num= 48040430 
+ #I wrote it as a very long integer when it could be an string of both  
+auto_policy_num = 5779 
+ #I wrote it as a 4 digit number when it could have been a string or one number 
+
+user_name = 'Smaya Melgara'
+user_city = 'Charlotte'
+user_state = 'North Carolina'
+
+import keyword
+
+print (keyword.kwlist)
+print (len(keyword.kwlist))
+
+#B. Definitions:
+# RETURN : The keyword is used to exit a function or a method and return some value.
+# TRY : Makes a TRY.. Starts a block of code that is tried to execute.
+# LAMBDA : an anonymous function. Can take any number of arguments but only have a single expression. 
+# ASSERT : used for debugging 
+# NONLOCAL : Used in functions inside functions to create anonymous functions. 
+

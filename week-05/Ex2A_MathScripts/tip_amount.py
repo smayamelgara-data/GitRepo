350f610255d635b3ef68e80f19062d8d511599e7
smayamelgara-data <smayamelgara@gmail.com>
2026-05-06 11:35:25 -0400

    Adding Tip Calculation Python File.

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,12 @@
+#How do you calculate the tip amount on a restaurant bill given the tip percentage?
+
+# Example values
+bill = 40
+tip_percent = 20
+
+# Calculate tip
+tip = bill * (tip_percent / 100)
+
+# Output
+print("The tip on a $" + str(bill) + " restaurant bill is $" + str(tip))
+

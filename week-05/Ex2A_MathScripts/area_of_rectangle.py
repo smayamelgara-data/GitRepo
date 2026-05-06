bea4331a5fa6a935b061bc0e358397a5fe3a4d2b
smayamelgara-data <smayamelgara@gmail.com>
2026-05-06 10:11:13 -0400

    Adding Area Calculation Python File.

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,15 @@
+#How do you calculate the area of a rectangle?
+
+#The area of a rectangle is calculated by length times width 
+
+#Side Lengths 
+side_a = 6
+side_b = 23 
+
+#Area calculation 
+area = side_a * side_b 
+
+#Output 
+print("Side A is", side_a)
+print("Side B is", side_b)
+print("The area of the rectangle is", area)

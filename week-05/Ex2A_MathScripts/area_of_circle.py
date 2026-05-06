02a5e774b124694c80378f775c4fc2816e227583
smayamelgara-data <smayamelgara@gmail.com>
2026-05-06 11:34:43 -0400

    Adding Area Of Calculation Python File.

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,13 @@
+#How do you calculate the area of a circle 
+
+# Diameter 
+diameter = 23
+
+# Convert to radius
+radius = diameter / 2  
+
+# Calculate area
+area = 3.14 * (radius ** 2)
+
+# Output
+print(f"The area of a circle with radius {radius} is {area:.3f}")

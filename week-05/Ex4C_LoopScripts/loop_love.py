65e5a1f55f82eec88a0f144180373c23a92c09f8
smayamelgara-data <smayamelgara@gmail.com>
2026-05-08 13:14:18 -0400

    Adding Loop love Python File.

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,13 @@
+#Exercie 4C Loop love
+#Lab 1
+
+# Create a list of words
+great_words = ["amazing", "awesome", "excellent", "fantastic", "incredible"]
+
+# Loop through the list
+for word in great_words:
+    print("Loops are", word + "!")
+
+# Final message
+print("I <3 loops")
+
